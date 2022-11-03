6d7b87ef899e2ed52bd4e2b4a9b89bad7f44d55f
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-11-03 19:06:33 +0000

    Set up RetroArch bindings using RetroCore trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
 
+use libretro_rs::{libretro_core, RetroCore, RetroEnvironment, RetroGame,
+    RetroLoadGameResult, RetroRuntime, RetroSystemInfo};
+
+struct Chip8Core;
+
+impl RetroCore for Chip8Core {
+    fn init(env: &RetroEnvironment) -> Self {
+        Chip8Core {}
+    }
+
+    fn get_system_info() -> RetroSystemInfo {
+        RetroSystemInfo::new("CHIP-8 Emulator", "0.1.0")
+    }
+
+    fn reset(&mut self, env: &RetroEnvironment) {
+
+    }
+
+    fn run(&mut self, env: &RetroEnvironment, runtime: &RetroRuntime) {
+
+    }
+
+    fn load_game(&mut self, env: &RetroEnvironment, game: RetroGame) -> RetroLoadGameResult {
+        RetroLoadGameResult::Failure
+    }
+}
+
+libretro_core!(Chip8Core);
