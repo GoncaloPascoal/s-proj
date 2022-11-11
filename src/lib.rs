5a134bf3ab0b6c2f2c785bf52eda20fc5e09101b
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-11-11 18:25:09 +0000

    Implemented ADDR, SHR and XOR instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::{Shr, ShrAssign}};
 
 use libretro_rs::{libretro_core, RetroCore, RetroEnvironment, RetroGame,
     RetroLoadGameResult, RetroRuntime, RetroSystemInfo};
@@ -28,18 +28,6 @@ impl Instruction {
     fn args(&self, instruction: u16) -> HashMap<&'static str, u16> {
         self.arg_masks.iter().map(|(&k, _)| (k, self.arg(instruction, k))).collect()
     }
-
-    fn todo(core: &mut Chip8Core, args: HashMap<&'static str, u16>) {
-        todo!()
-    }
-
-    /// Store `NN` in register `VX`
-    fn mov(core: &mut Chip8Core, args: HashMap<&'static str, u16>) {
-        let x = args.get("X").unwrap();
-        let n = args.get("N").unwrap();
-
-        core.cpu.registers[*x as usize] = *n as u8;
-    }
 }
 
 struct Cpu {
@@ -65,19 +53,37 @@ impl Cpu {
         instructions.insert(0x6000, Instruction {
             name: "MOV",
             arg_masks: HashMap::from([("X", Instruction::HEX_2), ("N", Instruction::HEX_01)]),
-            callback: Instruction::mov,
+            callback: Chip8Core::mov,
         });
 
         instructions.insert(0x8000, Instruction {
             name: "MOVR",
             arg_masks: HashMap::from([("X", Instruction::HEX_2), ("Y", Instruction::HEX_1)]),
-            callback: Instruction::todo,
+            callback: Chip8Core::todo,
+        });
+
+        instructions.insert(0x8003, Instruction {
+            name: "XOR",
+            arg_masks: HashMap::from([("X", Instruction::HEX_2), ("Y", Instruction::HEX_1)]),
+            callback: Chip8Core::xor,
+        });
+
+        instructions.insert(0x8004, Instruction {
+            name: "ADDR",
+            arg_masks: HashMap::from([("X", Instruction::HEX_2), ("Y", Instruction::HEX_1)]),
+            callback: Chip8Core::addr,
+        });
+
+        instructions.insert(0x8006, Instruction {
+            name: "SHR",
+            arg_masks: HashMap::from([("X", Instruction::HEX_2), ("Y", Instruction::HEX_1)]),
+            callback: Chip8Core::shr,
         });
 
         instructions.insert(0xA000, Instruction {
             name: "MOVI",
             arg_masks: HashMap::from([("N", Instruction::HEX_012)]),
-            callback: Instruction::todo,
+            callback: Chip8Core::todo,
         });
 
         instructions
@@ -86,6 +92,56 @@ impl Cpu {
 
 struct Chip8Core {
     cpu: Cpu,
+}
+
+impl Chip8Core {
+    fn todo(_core: &mut Chip8Core, _args: HashMap<&'static str, u16>) {
+        todo!()
+    }
+
+    /// Add value of register `VY` to register `VX`. Set `VF` to `01` if carry
+    /// occurs, `00` otherwise.
+    fn addr(&mut self, args: HashMap<&'static str, u16>) {
+        let x = *args.get("X").unwrap() as usize;
+        let y = *args.get("Y").unwrap() as usize;
+
+        let x_val = self.cpu.registers[x];
+        let y_val = self.cpu.registers[y];
+
+        let (result, carry) = x_val.overflowing_add(y_val);
+
+        self.cpu.registers[x] = result;
+        self.cpu.registers[0xF] = carry as u8;
+    }
+
+    /// Store `NN` in register `VX`.
+    fn mov(&mut self, args: HashMap<&'static str, u16>) {
+        let x = *args.get("X").unwrap() as usize;
+        let n = *args.get("N").unwrap() as u8;
+
+        self.cpu.registers[x] = n;
+    }
+
+    /// Store value of `VY` in `VX` shifted right one bit. Set `VF` to least
+    /// significant bit prior to shift.
+    fn shr(&mut self, args: HashMap<&'static str, u16>) {
+        let x = *args.get("X").unwrap() as usize;
+        let y = *args.get("Y").unwrap() as usize;
+
+        let y_val = self.cpu.registers[y];
+
+        // Store least significant bit in VF
+        self.cpu.registers[0xF] = y_val & 0x01;
+        self.cpu.registers[x] = y_val >> 1;
+    }
+
+    /// Set `VX` to `VX` XOR `VY`.
+    fn xor(&mut self, args: HashMap<&'static str, u16>) {
+        let x = *args.get("X").unwrap() as usize;
+        let y = *args.get("Y").unwrap() as usize;
+
+        self.cpu.registers[x] ^= self.cpu.registers[y];
+    }
 }
 
 impl RetroCore for Chip8Core {
