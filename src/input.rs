c097306540dba8a2e10c3694844b68df2e9acd6f
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-12-11 18:29:57 +0000

    Reorder Chip8Key variants for consistency with CHIP-8 keypad

--- src/input.rs
+++ src/input.rs
@@ -3,20 +3,20 @@ use strum_macros::EnumIter;
 
 #[derive(EnumIter)]
 pub enum Chip8Key {
+    X    = 120,
     Num1 = 49,
     Num2 = 50,
     Num3 = 51,
-    Num4 = 52,
     Q    = 113,
     W    = 119,
     E    = 101,
-    R    = 114,
     A    = 97,
     S    = 115,
     D    = 100,
-    F    = 102,
     Z    = 122,
-    X    = 120,
     C    = 99,
+    Num4 = 52,
+    R    = 114,
+    F    = 102,
     V    = 118,
 }
